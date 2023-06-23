45a04be9fa8a14c1ad9b44c9cd0e21a5b97d7e1a
luigimariapascali <luigimariapascali@gmail.com>
2023-06-23 08:50:10 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -19,48 +19,118 @@ function generateBoard(difficulty) {
         cols = 7;
         maxNum = 49;
         break;
-      default:
-        rows = 10;
-        cols = 10;
-        maxNum = 100;
     }
+  
+    // Generazione delle bombe:
+    const bombs = generateBombs(difficulty, maxNum);
+    const maxScore = rows * cols - bombs.length;
+    let score = 0;
+    let gameOver = false;
+  
+    // Seleziona la card dei game stats:
+    const gameStatsCard = document.querySelector('.card');
+  
+    // Aggiorna i contatori nella card:
+    const bombCounter = gameStatsCard.querySelector('#bomb-counter');
+    const pointCounter = gameStatsCard.querySelector('#point-counter');
+    
+    bombCounter.textContent = bombs.length;
+    pointCounter.textContent = score;
+    
   
     for (let row = 1; row <= rows; row++) {
       // Creazione di un elemento div per rappresentare una riga:
       const rowElem = document.createElement('div');
       rowElem.className = 'row';
-    
+  
       for (let col = 1; col <= cols; col++) {
         // Creazione di un elemento div per rappresentare una cella:
         const cell = document.createElement('div');
         cell.className = 'col-1 cell';
-    
+  
         // Assegnazione del testo alla cella corrente basato sulla posizione nella griglia:
         cell.innerText = (row - 1) * cols + col;
-    
+  
         // Aggiunta dell'evento di click alla cella
         cell.addEventListener('click', handleClick);
-    
+  
         // Aggiunta della cella alla riga corrente:
         rowElem.appendChild(cell);
       }
-    
+  
       // Aggiunta della riga al contenitore della griglia:
       board.appendChild(rowElem);
     }
   
     // Mostra il bottone di reset:
     document.getElementById('reset-button').style.display = 'block';
-  }
   
-  function handleClick(event) {
-    const cell = event.target;
+    function generateBombs(difficulty, maxNum) {
+      const bombs = [];
+      const maxBombs = 16;
+      let i = 0;
   
-    // Aggiunta della classe 'clicked' alla cella cliccata:
-    cell.classList.add('clicked');
+      while (i < maxBombs) {
+        const bomb = Math.floor(Math.random() * maxNum) + 1;
+        if (!bombs.includes(bomb)) {
+          bombs.push(bomb);
+          i++;
+        }
+      }
   
-    // Stampa del testo della cella cliccata nella console:
-    console.log('Cella cliccata:', cell.innerText);
+      return bombs;
+    }
+  
+    function handleClick(event) {
+      if (gameOver) {
+        return;
+      }
+  
+      const cell = event.target;
+      const cellNum = parseInt(cell.innerText);
+      const lossCounter = document.getElementById('loss-counter');
+ 
+      if (bombs.includes(cellNum)) {
+        // Se clicco su una bomba il gioco finisce:
+        cell.classList.add('bomb');
+        alert('Game over! Your score is ' + score);
+        gameOver = true;
+        removeClickEvent();
+        
+        // Mostra tutte le bombe:
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+          const cellNum = parseInt(cell.innerText);
+          if (bombs.includes(cellNum)) {
+            cell.classList.add('bomb');
+          }
+        });
+        lossCounter.textContent = parseInt(lossCounter.textContent) + 1;
+      } else {
+        // Altrimenti aumento il punteggio:
+        cell.classList.add('clicked');
+        score++;
+  
+        if (score === maxScore) {
+          // Se non clicco su nessuna bomba ho vinto:
+          alert('Congratulations! You won! Your score is ' + score);
+          gameOver = true;
+          removeClickEvent();
+        }
+      }
+  
+      // Aggiorna i contatori nella card:
+      bombCounter.textContent = bombs.length;
+      pointCounter.textContent = score;
+      lossCounter.textContent = gameOver ? 1 : 0;
+    }
+  
+    function removeClickEvent() {
+      const cells = document.querySelectorAll('.cell');
+      cells.forEach(cell => {
+        cell.removeEventListener('click', handleClick);
+      });
+    }
   }
   
   // Seleziona i bottoni di difficoltà:
@@ -104,11 +174,10 @@ function generateBoard(difficulty) {
     easyButton.disabled = false;
     mediumButton.disabled = false;
     hardButton.disabled = false;
-  
+      
     // Nascondi il bottone di reset:
     resetButton.style.display = 'none';
   
     // Resetta la griglia:
     generateBoard(1);
   });
-  
